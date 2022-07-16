bdd005f0a6acc885b4568992f947c501138127c5
= <=>
2022-07-16 18:40:19 +0500

    new changes

--- src/pages/NewQuestions.js
+++ src/pages/NewQuestions.js
@@ -1,5 +1,44 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+import { Divider, Avatar } from "@mui/material/";
+import { useState, useEffect } from "react";
+
+import { _getQuestions } from "../data/_DATA";
+
 const NewQuestions = () => {
-  return <h1>New Questions</h1>;
+  const [questions, setQuestions] = useState();
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const res = await _getQuestions();
+      const data = Object.values(res);
+      setQuestions([...data]);
+    };
+    fetchQuestions();
+  }, []);
+
+  return (
+    <>
+      {questions && (
+        <List>
+          {questions.map((item) => (
+            <div key={item.id}>
+              <ListItem>
+                <Avatar src="'https://www.w3schools.com/w3images/avatar2.png'" />
+                <ListItemText>
+                  Would you rather {item.optionOne.text} or{" "}
+                  {item.optionTwo.text}
+                </ListItemText>
+                <p>
+                  {item.author} <span>on {item.timestamp}</span>
+                </p>
+              </ListItem>
+              <Divider />
+            </div>
+          ))}
+        </List>
+      )}
+    </>
+  );
 };
 
 export default NewQuestions;
